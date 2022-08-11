113f759d6ec973392af0882432a9e78817b15579
Md. Abu Sufian Sweet <61094774+Md-Abu-Sufian-Sweet@users.noreply.github.com>
2022-08-11 19:52:35 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+ AOS.init();
+
+
+
+
+
+
+
+
+
+// scroll-top//
+let mybutton = document.getElementById("btn-back-to-top");
+
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+// scroll-top//
